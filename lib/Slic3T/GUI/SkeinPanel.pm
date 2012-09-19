13b62352d509ce539ea16a3c572f53650c629e4e
Sublime <Intrinsicallysublime@yahoo.ca>
2012-09-19 00:26:11 -0700

    Remove retraction speed for GUI

--- lib/Slic3T/GUI/SkeinPanel.pm
+++ lib/Slic3T/GUI/SkeinPanel.pm
@@ -48,7 +48,7 @@ sub new {
         },
         retract => {
             title => 'Retraction',
-            options => [qw(retract_length retract_lift retract_speed retract_restart_extra retract_before_travel)],
+            options => [qw(retract_length retract_lift retract_restart_extra retract_before_travel)],
         },
         cooling => {
             title => 'Cooling',
